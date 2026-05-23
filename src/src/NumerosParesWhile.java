04c8cf3cd7079e69c98fb0dfad41a45f5bd96844
Joselin Herrera <joselinflores34@outlook.com>
2026-05-22 19:40:04 -0600

    Añadir ejercicio NumerosParesWhile

--- src/src/NumerosParesWhile.java
+++ src/src/NumerosParesWhile.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class NumerosParesWhile {
+
+    public static void main(String[] args) {
+
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero, contador = 0;
+
+
+        System.out.println("Ingrese un número:");
+        numero = entrada.nextInt();
+
+
+        while (contador <= numero) {
+
+            if (contador % 2 == 0) {
+                System.out.println(contador);
+            }
+
+            contador++;
+        }
+    }
 }
